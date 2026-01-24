03ced4682cbfaf45d278849be2c0a2056c924cc6
Elton Kola <eltonkola@gmail.com>
2026-01-23 20:20:14 -0500

    target 24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mrkola.kidztv"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
